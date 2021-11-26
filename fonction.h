e242a2b787210c7bf611775bf91402eb4e75906a
Vinicius V. Careli <vcareli@gmail.com>
2021-11-26 13:22:57 -0500

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -1,7 +1,9 @@
 #ifndef __FONCTION_H_
 #define __FONCTION_H_
+#define TAILLE_MOT 50
 
 char lirecaracter();
-void affiche_mot(int tamanho, int palavra[]);
+int recherche_lettre(char lettre, char mot[], int tableau[]);
+int gagner(int tableau[], long longueur);
 
 #endif // __FONCTION_H_
